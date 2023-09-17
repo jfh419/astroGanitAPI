b7a51b1631d89833d3e3393b728e7447a4b0a54f
JITENDRA <JITENDRA@LAPTOP-JE9BTSTL>
2023-09-17 13:51:07 +0530

    yearlyhoroscope

---
+++ astroGanitAPI/src/main/java/com/astroganit/api/constant/HoroscopeConstant2024.java
@@ -0,0 +1,102 @@
+package com.astroganit.api.constant;
+
+public class HoroscopeConstant2024 {
+
+	//"शिक्षा","विवाह","फाइनेंस","व्यापार","करियर","प्रेम","स्वास्थ्य"
+	
+	public static String [] ariesYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] taurusYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] geminiYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] cancerYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] leoYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] virgoYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] libraYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] scorpiusYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] sagittariusYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] capricornusYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] aquariusYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+	
+	public static String [] piscesYH= {"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			""};
+}
---
+++ astroGanitAPI/src/main/java/com/astroganit/api/payload/YearlyHoroscopeAspect24.java
@@ -0,0 +1,60 @@
+package com.astroganit.api.payload;
+
+public class YearlyHoroscopeAspect24 {
+	//"शिक्षा","विवाह","फाइनेंस","व्यापार","करियर","प्रेम","स्वास्थ्य"
+	private String education;
+	private String marriage;
+	private String finance;
+	private String business;
+	private String carrer;
+	private String love;
+	private String health;
+	
+	
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getMarriage() {
+		return marriage;
+	}
+	public void setMarriage(String marriage) {
+		this.marriage = marriage;
+	}
+	public String getFinance() {
+		return finance;
+	}
+	public void setFinance(String finance) {
+		this.finance = finance;
+	}
+	public String getBusiness() {
+		return business;
+	}
+	public void setBusiness(String business) {
+		this.business = business;
+	}
+	public String getCarrer() {
+		return carrer;
+	}
+	public void setCarrer(String carrer) {
+		this.carrer = carrer;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+	public String getHealth() {
+		return health;
+	}
+	public void setHealth(String health) {
+		this.health = health;
+	}
+	
+	
+	
+		
+}
